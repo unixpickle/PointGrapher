52ff8c0564d4fa9ebee94d9cb01b0831d7f76ee9
Alex Nichol <alexnichol@comcast.net>
2011-10-29 10:25:30 -0400

    Initial Commit

---
+++ PointGrapher/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  PointGrapher
+//
+//  Created by Alex Nichol on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ PointGrapher/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PointGrapher
+//
+//  Created by Alex Nichol on 10/29/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
